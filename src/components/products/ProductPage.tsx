2a882ef564f5d092791c87da9d7e4af05a275c8d
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-02 14:24:46 +0800

    Add error handling when no product is found in data base when clicking on product for product page.

--- src/components/products/ProductPage.tsx
+++ src/components/products/ProductPage.tsx
@@ -5,17 +5,33 @@ import { IProductProperties } from "../../types/productTypes";
 
 export default function ProductPage() {
     const [productDetails, setProductDetails] = useState<IProductProperties>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [productFound, setProductFound] = useState(false);
     const { _id } = useParams();
 
     useEffect(() => {
         if (_id) {
             const getDetails = async () => {
                 const returnedDetails = await useProductDetails(_id);
+                if (returnedDetails.message) {
+                    setProductFound(false);
+                    setIsLoading(false);
+                    return;
+                }
                 setProductDetails(returnedDetails);
+                setProductFound(true);
+                setIsLoading(false);
             };
             getDetails();
         }
     }, []);
 
-    return <div>{productDetails?._id}</div>;
+    return (
+        <div>
+            {isLoading && "Loading. . ."}
+            <div>{productDetails?._id}</div>
+            <div>{productFound && "true"}</div>
+            <div>{!productFound && "Product Not Found"}</div>
+        </div>
+    );
 }
