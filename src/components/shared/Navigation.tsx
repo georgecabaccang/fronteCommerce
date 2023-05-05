285d5f7caa0bc9b4422e72d8d4ae984ac65c39ff
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-05 13:35:08 +0800

    Change how NavLinks appear on specific active page and if logged in or not

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -1,6 +1,11 @@
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../providers/UserProvider";
 
 export default function Navigation() {
+    const [activeLink, setActiveLink] = useState("home");
+    const userContext = useContext(UserContext);
+
     return (
         <div className="bg-orange-100 h-[3em] sticky top-0 z-10">
             <div className="flex mx-10 h-full items-center">
@@ -11,33 +16,56 @@ export default function Navigation() {
                     <NavLink
                         to={"/"}
                         className="flex place-content-center items-center w-full h-full link-page"
+                        onClick={() => setActiveLink("home")}
                     >
                         Home
                     </NavLink>
                     <NavLink
                         to={"/shop"}
                         className="flex place-content-center items-center w-full h-full link-page"
+                        onClick={() => setActiveLink("shop")}
                     >
                         Shop
                     </NavLink>
-                    <NavLink
-                        to={"/cart"}
-                        className="flex place-content-center items-center w-full h-full link-page"
-                    >
-                        Cart
-                    </NavLink>
-                    <NavLink
-                        to={"/login"}
-                        className="flex place-content-center items-center w-full h-full link-page"
-                    >
-                        Login
-                    </NavLink>
-                    <NavLink
-                        to={"/sign-up"}
-                        className="flex place-content-center items-center w-full h-full link-page"
-                    >
-                        Sign Up
-                    </NavLink>
+                    {userContext.accessToken && (
+                        <NavLink
+                            to={"/cart"}
+                            className="flex place-content-center items-center w-full h-full link-page"
+                            onClick={() => setActiveLink("cart")}
+                        >
+                            Cart
+                        </NavLink>
+                    )}
+                    {!userContext.accessToken && (
+                        <>
+                            {activeLink === "sign-up" ||
+                            activeLink === "home" ||
+                            activeLink === "shop" ? (
+                                <NavLink
+                                    to={"/login"}
+                                    className="flex place-content-center items-center w-full h-full link-page"
+                                    onClick={() => setActiveLink("login")}
+                                >
+                                    Login
+                                </NavLink>
+                            ) : (
+                                ""
+                            )}
+                            {activeLink === "login" ||
+                            activeLink === "home" ||
+                            activeLink === "shop" ? (
+                                <NavLink
+                                    to={"/sign-up"}
+                                    className="flex place-content-center items-center w-full h-full link-page"
+                                    onClick={() => setActiveLink("sign-up")}
+                                >
+                                    Sign Up
+                                </NavLink>
+                            ) : (
+                                ""
+                            )}
+                        </>
+                    )}
                 </nav>
             </div>
         </div>
