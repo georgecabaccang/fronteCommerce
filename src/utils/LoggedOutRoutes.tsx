09591d06498b65a101ba5b689594ab57a2cc17c0
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-08 13:40:39 +0800

    Fix bug always showing Sign Up tab when refresing in current page

--- src/utils/LoggedOutRoutes.tsx
+++ src/utils/LoggedOutRoutes.tsx
@@ -4,13 +4,16 @@ import { Navigate, Outlet } from "react-router-dom";
 import { ActiveLinkContext } from "../providers/ActiveLinkProvider";
 
 const LOGIN_LINK = "http://localhost:5173/login";
+const SIGN_UP_LINK = "http://localhost:5173/sign-up";
 
 export default function LoggedOutRoutes() {
     const userContext = useContext(UserContext);
     const activeLinkContext = useContext(ActiveLinkContext);
 
     useEffect(() => {
-        activeLinkContext.setActiveLink(LOGIN_LINK);
+        if (window.location.href == LOGIN_LINK) return activeLinkContext.setActiveLink(LOGIN_LINK);
+        if (window.location.href == SIGN_UP_LINK)
+            return activeLinkContext.setActiveLink(SIGN_UP_LINK);
     }, []);
 
     return !userContext.accessToken && !userContext.refreshToken ? <Outlet /> : <Navigate to="/" />;
