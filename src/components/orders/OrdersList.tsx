52716971cd5f8648152d3d137342f4ec320ef9b0
georgesycabaccang <georgesycabaccang@gmail.com>
2023-05-29 10:51:45 +0800

    Run npm build

--- src/components/orders/OrdersList.tsx
+++ src/components/orders/OrdersList.tsx
@@ -1,25 +1,25 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { OrdersContext } from "../../providers/OrdersProvider";
 import Orders from "./Orders";
-import Button from "../shared/Button";
-import { IOrder } from "../../types/orderTypes";
+// import Button from "../shared/Button";
+// import { IOrder } from "../../types/orderTypes";
 
-const filterByReceived = "received";
+// const filterByReceived = "received";
 
 export default function OrdersList() {
     // const [filterOrder, setFilterOrder] = useState<IOrder>();
     const ordersContext = useContext(OrdersContext);
 
-    const filterBy = (filterBy: string) => {
-        let filteredOrders;
-        if (filterBy === filterByReceived) {
-            filteredOrders = ordersContext.orders.filter((order) => {
-                return order.status === filterByReceived;
-            });
-        }
+    // const filterBy = (filterBy: string) => {
+    //     let filteredOrders;
+    //     if (filterBy === filterByReceived) {
+    //         filteredOrders = ordersContext.orders.filter((order) => {
+    //             return order.status === filterByReceived;
+    //         });
+    //     }
 
-        // setFilterOrder(filteredOrders);
-    };
+    //     // setFilterOrder(filteredOrders);
+    // };
 
     if (!ordersContext.orders) {
         return <div>Loading . . .</div>;
@@ -27,12 +27,12 @@ export default function OrdersList() {
 
     return (
         <div className="mx-10">
-            <Button
+            {/* <Button
                 name="Received"
                 onClick={() => {
                     filterBy(filterByReceived);
                 }}
-            />
+            /> */}
             {ordersContext.orders.map((order) => {
                 return (
                     <Orders
